68e8b7adfebe7d0a9590516e99b14445f6039f8d
Lukas Seier <lukas.seier12@gmail.com>
2024-08-26 20:43:57 +0100

    Changed some gui settings

--- home.nix
+++ home.nix
@@ -7,7 +7,7 @@
   home.packages = with pkgs; [
     discord
     gh
-    okular
+    #okular # pdf viewer
     spotify
     vscode
   ];
@@ -17,6 +17,16 @@
     userName = "Lukas Seier";
     userEmail = "lukas.seier12@gmail.com";
     aliases = {};
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+    };
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+    };
   };
 
   home.stateVersion = "24.05";
