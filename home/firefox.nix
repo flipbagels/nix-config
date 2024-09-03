1df0d24b906a364d35e5e2a8c3b5a5a2ca472ae3
Lukas Seier <lukas.seier12@gmail.com>
2024-09-03 12:51:05 +0100

    Added configuration of extensions in firefox toolbar

--- home/firefox.nix
+++ home/firefox.nix
@@ -10,7 +10,7 @@
         name = "default";
         isDefault = true;
         extensions = with flake-inputs.firefox-addons.packages."x86_64-linux"; [
-          ublock-origin
+          # ublock-origin
           zotero-connector
         ];
         settings = {
@@ -18,9 +18,9 @@
           "extensions.autoDisableScopes" = 0;
           "extensions.pocket.enabled" = false;
         };
-        extraConfig =
-          # Configure which extensions are pinned to toolbar
-          ''user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"ublock0_raymondhill_net-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\",\"zotero_chnm_gmu_edu-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"zotero_chnm_gmu_edu-browser-action\",\"ublock0_raymondhill_net-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":20,\"newElementCount\":3}");'';
+        # extraConfig =
+        #   # Configure which extensions are pinned to toolbar
+        #   ''user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"ublock0_raymondhill_net-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\",\"zotero_chnm_gmu_edu-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"zotero_chnm_gmu_edu-browser-action\",\"ublock0_raymondhill_net-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":20,\"newElementCount\":3}");'';
       };
     };
   };
