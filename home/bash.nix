7211ac20c4d1b97095bf10617fb03e591ed4d19f
Lukas Seier <lukas.seier12@gmail.com>
2024-09-11 14:51:22 +0100

    Added some shell aliases

--- home/bash.nix
+++ home/bash.nix
@@ -5,7 +5,9 @@
     enable = true;
 
     shellAliases = {
+      cf = "cd /etc/nixos";
       oxvpn = ''sudo echo \"Sudo first\" && openconnect https://vpn.ox.ac.uk --useragent=AnyConnect --cookieonly | sudo openconnect https://vpn.ox.ac.uk --useragent=AnyConnect --gnutls-priority="NORMAL:-VERS-ALL:+VERS-TLS1.2:+RSA:+AES-128-CBC:+SHA1" --cookie-on-stdin'';
+      rb = "sudo nixos-rebuild switch";
     };
   };
 }
