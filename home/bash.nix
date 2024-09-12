2df881cdf4ed5c171886c5a30b6f8c0666aaf616
Lukas Seier <lukas.seier12@gmail.com>
2024-09-12 20:47:22 +0100

    Added bash aliases for git

--- home/bash.nix
+++ home/bash.nix
@@ -6,6 +6,14 @@
 
     shellAliases = {
       cf = "cd /etc/nixos";
+      df = "df -h";
+      ga = "git add";
+      gc = "git commit";
+      gd = "git diff";
+      gg = "git pull";
+      gp = "git push";
+      gr = "git reset";
+      gs = "git status";
       oxvpn = ''sudo echo \"Sudo first\" && openconnect https://vpn.ox.ac.uk --useragent=AnyConnect --cookieonly | sudo openconnect https://vpn.ox.ac.uk --useragent=AnyConnect --gnutls-priority="NORMAL:-VERS-ALL:+VERS-TLS1.2:+RSA:+AES-128-CBC:+SHA1" --cookie-on-stdin'';
       rb = "sudo nixos-rebuild switch";
     };
--- home/firefox.nix
+++ home/firefox.nix
@@ -10,6 +10,7 @@
         name = "default";
         isDefault = true;
         extensions = with flake-inputs.firefox-addons.packages."x86_64-linux"; [
+          darkreader
           ublock-origin
           zotero-connector
         ];
@@ -20,7 +21,7 @@
         };
         extraConfig =
           # Configure which extensions are pinned to toolbar
-          ''user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"ublock0_raymondhill_net-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\",\"zotero_chnm_gmu_edu-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"zotero_chnm_gmu_edu-browser-action\",\"ublock0_raymondhill_net-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":20,\"newElementCount\":3}");'';
+          ''user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"fxa-toolbar-menu-button\",\"unified-extensions-button\",\"zotero_chnm_gmu_edu-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"import-button\",\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"zotero_chnm_gmu_edu-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"addon_darkreader_org-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"toolbar-menubar\",\"TabsToolbar\",\"unified-extensions-area\"],\"currentVersion\":20,\"newElementCount\":3}");'';
       };
     };
   };
