bc45aa88f04e17825166e03e0025979783a35f92
Lukas Seier <lukas.seier12@gmail.com>
2025-01-19 00:07:01 +0000

    Fixed issue with vscode extensions not showing

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -6,15 +6,67 @@
   programs.vscode = {
     enable = true;
 
-    extensions = with pkgs.vscode-marketplace; [
-      jnoortheen.nix-ide
-      mechatroner.rainbow-csv
-      ms-python.python
-      ms-toolsai.jupyter
-      ms-vscode-remote.remote-ssh
-      sumneko.lua
-      vscodevim.vim
+    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "tokyo-night";
+        publisher = "enkia";
+        version = "1.0.9";
+        sha256 = "2EphqiRVFw5cHAfCS+wscUM5eR48W9y96LJZQH4k0l4=";
+      }
+      {
+        name = "vim";
+        publisher = "vscodevim";
+        version = "1.29.0";
+        sha256 = "J3V8SZJZ2LSL8QfdoOtHI1ZDmGDVerTRYP4NZU17SeQ=";
+      }
+      {
+        name = "nix";
+        publisher = "bbenoist"; 
+        version = "1.0.1";
+        sha256 = "qwxqOGublQeVP2qrLF94ndX/Be9oZOn+ZMCFX1yyoH0=";
+      }
+      {
+        name = "lua";
+        publisher = "sumneko";
+        version = "3.13.5";
+        sha256 = "WKgVHkpY6xjPXcgKl3V4D4tmb2W+1+IN3gedHZ5SRuU=";
+      }
+      {
+        name = "python";
+        publisher = "ms-python";
+        version = "2024.22.2";
+        sha256 = "uVv4kpTf0W82Gvoju0Q/HKf6SpN2mwuYO7NItlRoezI=";
+      }
+      {
+        name = "jupyter";
+        publisher = "ms-toolsai";
+        version = "2024.11.0";
+        sha256 = "BwZLF52ilEaLU6swUFEa+PtrxzsFOLIYd8TVD7xRgPg=";
+      }
+      {
+        name = "rainbow-csv";
+        publisher = "mechatroner";
+        version = "3.14.0";
+        sha256 = "yNX2l8s99ySP/g5IDQ1G5kk8X2ujxx+Lc91irHkqPqI=";
+      }
+      {
+        name = "remote-ssh";
+        publisher = "ms-vscode-remote";
+        version = "0.116.1";
+        sha256 = "onDOybZsYMZltaVDP70quvZ3pejPsc0qvM+e2aLisXQ=";
+      }
     ];
+
+    # extensions = with pkgs.vscode-marketplace; [
+    #   enkia.tokyo-night
+    #   jnoortheen.nix-ide
+    #   mechatroner.rainbow-csv
+    #   ms-python.python
+    #   ms-toolsai.jupyter
+    #   ms-vscode-remote.remote-ssh
+    #   sumneko.lua
+    #   vscodevim.vim
+    # ];
 
     userSettings = {
       "editor.lineNumbers" = "relative";
@@ -22,6 +74,7 @@
       "update.mode" = "none";
       "vim.useSystemClipboard" = true;
       "jupyter.askForKernelRestart" = false;
+      "editor.minimap.enabled" = false;
     };
   };
 }
